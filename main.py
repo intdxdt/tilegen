5188a248c2b2347dd37be1abe1f8280e8e414e50
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-22 00:01:59 -0300

    doc

--- main.py
+++ main.py
@@ -2,40 +2,11 @@ import gdal
 import sys
 from gdal2tiles import GDAL2Tiles
 
-# =============================================================================
-# =============================================================================
-# =============================================================================
-
+#TODO:
 __doc__globalmaptiles = """
-globalmaptiles.py
-
-Global Map Tiles as defined in Tile Map Service (TMS) Profiles
-==============================================================
-
-Functions necessary for generation of global tiles used on the web.
-It contains classes implementing coordinate conversions for:
-
-  - GlobalMercator (based on EPSG:900913 = EPSG:3785)
-       for Google Maps, Yahoo Maps, Bing Maps compatible tiles
-  - GlobalGeodetic (based on EPSG:4326)
-       for OpenLayers Base Map and Google Earth compatible tiles
-
-More info at:
-
-http://wiki.osgeo.org/wiki/Tile_Map_Service_Specification
-http://wiki.osgeo.org/wiki/WMS_Tiling_Client_Recommendation
-http://msdn.microsoft.com/en-us/library/bb259689.aspx
-http://code.google.com/apis/maps/documentation/overlays.html#Google_Maps_Coordinates
-
-Created by Klokan Petr Pridal on 2008-07-03.
-Google Summer of Code 2008, project GDAL2Tiles for OSGEO.
-
 In case you use this class in your product, translate it to another language
 or find it useful for your project please let me know.
 My email: klokan at klokan dot cz.
-I would like to know where it was used.
-
-Class is available under the open-source GDAL license (www.gdal.org).
 """
 
 
